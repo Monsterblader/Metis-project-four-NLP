b9baae5d5692ac2d8b9e75c83b0add1a74faa2ec
Monsterblader <Monsterblader@gmail.com>
2020-11-13 02:08:43 -0800

    Adds rough draft of presentation.

--- HTML/js/script.js
+++ HTML/js/script.js
@@ -21,6 +21,14 @@ const showSlides = n => {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
 
+  switch (slideIndex) {
+    case 2:
+      d3.select('.body-background').style('background-image', "url('../images/background.png')");
+      break;
+    default:
+      d3.select('.body-background').style('background-image', "url('')");
+  }
+
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -29,6 +37,37 @@ const showSlides = n => {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+const matchTopics = ref => {
+  d3.selectAll(ref + ' tr.match.one')
+    .transition()
+    .duration(2000)
+    .style('background-color', 'maroon');
+  setTimeout(() => {
+    d3.selectAll(ref + ' tr.match.two')
+      .transition()
+      .duration(2000)
+      .style('background-color', 'darkgreen');
+  }, 1000);
+  setTimeout(() => {
+    d3.selectAll(ref + ' tr.match.three')
+      .transition()
+      .duration(2000)
+      .style('background-color', 'purple');
+  }, 2000);
+  setTimeout(() => {
+    d3.selectAll(ref + ' tr.match.four')
+      .transition()
+      .duration(2000)
+      .style('background-color', 'darkgrey');
+  }, 3000);
+  setTimeout(() => {
+    d3.selectAll(ref + ' tr.match.five')
+      .transition()
+      .duration(2000)
+      .style('background-color', 'orange');
+  }, 4000);
 }
 
 let slideIndex = 1;
